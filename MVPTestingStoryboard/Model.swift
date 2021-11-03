47f86ede6262f240e7377d25fb8bc860f8c4e3ee
Alex Rybachev <rybachev.as@gmail.com>
2021-11-03 21:51:31 +0400

    add Model

---
+++ MVPTestingStoryboard/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  MVPTestingStoryboard
+//
+//  Created by Aleksandr Rybachev on 03.11.2021.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+}
